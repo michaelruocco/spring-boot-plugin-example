2ec1fab6d588eb479610b06fae9caf5342f48c02
Michael Ruocco <michael.ruocco@hotmail.com>
2018-11-13 08:21:05 +0000

    removes application plugin runner

--- app/src/main/java/uk/co/mruoc/app/Application.java
+++ app/src/main/java/uk/co/mruoc/app/Application.java
@@ -1,10 +1,8 @@
 package uk.co.mruoc.app;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
@@ -13,10 +11,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public ApplicationRunner run() {
-        return new PluginApplicationRunner();
-    }
-
+    
 }
